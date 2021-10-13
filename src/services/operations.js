61b8dbb8ffcc9421a84e32e4fc9ee2394493d7ed
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-10-13 18:45:06 -0300

    coments operations

--- src/services/operations.js
+++ src/services/operations.js
@@ -1,16 +1,41 @@
 // Exporta variavel (funcao, const, let)
+
+/**
+ * Realiza Soma
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
 export const sum = (a, b) => {
     return a + b;
 }
 
+/**
+ * Realiza Subtracao
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
 export const subtract = (a, b) => {
     return a - b;
 }
 
+/**
+ * Realiza Multiplicacao
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
 export const multiplies = (a, b) => {
     return a * b;
 }
 
+/**
+ * Realiza divisao
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
 export const divide = (a, b) => {
     return a / b
 }
